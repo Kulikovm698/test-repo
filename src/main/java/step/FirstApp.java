7cda1300ad759f2935ab9c82ac5bfa13f751574d
Maxim Kulikov <makulikov@vtb.ru>
2022-11-20 23:57:22 +0300

    "Hello IDE"

--- src/main/java/step/FirstApp.java
+++ src/main/java/step/FirstApp.java
@@ -5,6 +5,6 @@ public class FirstApp {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello Java");
+        System.out.println("Hello IDE");
     }
 }
