0c4901be85fa7b38b5a3635a3c605f368fa3e68e
Maxim Kulikov <makulikov@vtb.ru>
2022-11-20 23:49:10 +0300

    some code

---
+++ src/main/java/step/FirstApp.java
@@ -0,0 +1,10 @@
+package src.main.java.step;
+
+
+public class FirstApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Java");
+    }
+}
